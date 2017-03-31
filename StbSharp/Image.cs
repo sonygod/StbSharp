bdf2164bcc6e7142cd8f0e41d8ae8db62aaf4b18
Roman Shapiro <eleroman@mail.ru>
2017-03-31 14:04:01 +0700

    README.md update

--- StbSharp/Image.cs
+++ StbSharp/Image.cs
@@ -7,5 +7,27 @@
 		public int SourceComp { get; set; }
 		public int Comp { get; set; }
 		public byte[] Data { get; set; }
+
+		public unsafe Image CreateResized(int newWidth, int newHeight)
+		{
+			var result = new Image
+			{
+				Comp = Comp,
+				SourceComp = SourceComp,
+				Data = new byte[newWidth*newHeight*Comp],
+				Width = newWidth,
+				Height = newHeight
+			};
+
+			fixed (byte* input = Data)
+			{
+				fixed (byte* output = result.Data)
+				{
+					Stb.stbir_resize_uint8(input, Width, Height, Width*Comp, output, newWidth, newHeight, newWidth*Comp, Comp);
+				}
+			}
+
+			return result;
+		}
 	}
 }
